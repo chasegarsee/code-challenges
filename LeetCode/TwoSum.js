9624106a59e33f59502dfeb7c022cda0cabe3d19
chasegarsee <chasegarseesoftwaredevelopment@gmail.com>
2019-09-04 11:06:18 -0400

    Two Sum

---
+++ LeetCode/TwoSum.js
@@ -0,0 +1,12 @@
+const nums = [2, 3, 4];
+const target = 7;
+
+const twoSum = function(nums, target) {
+  const comp = {};
+  for (let i = 0; i < nums.length; i++) {
+    if (comp[nums[i]] >= 0) {
+      return [comp[nums[i]], i];
+    }
+    comp[target - nums[i]] = i;
+  }
+};
