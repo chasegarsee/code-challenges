8728cef4ccc5a4e861be302c8728f6320c773a5e
chasegarsee <chasegarseesoftwaredevelopment@gmail.com>
2019-09-11 11:13:47 -0500

    capitalize 2.0

--- Lambda/capitalize.js
+++ Lambda/capitalize.js
@@ -27,3 +27,16 @@ function capitalize(str) {
   }
   return str;
 }
+
+function capitalizeTwoPointO(str) {
+  let result = str[0].toUpperCase();
+
+  for (let i = 1; i < str.length; i++) {
+    if (str[i - 1] === " ") {
+      result += str[i].toUpperCase();
+    } else {
+      result += str[i];
+    }
+  }
+  return result;
+}
