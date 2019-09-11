57e2935ceb848b46aea2f8aac5e1b3929f72120d
chasegarsee <chasegarseesoftwaredevelopment@gmail.com>
2019-09-11 11:00:29 -0500

    new folder + palindrom

---
+++ Lambda/palindrome.js
@@ -0,0 +1,17 @@
+// --- Directions
+// Given a string, return true if the string is a palindrome
+// or false if it is not.  Palindromes are strings that
+// form the same word if it is reversed. *Do* include spaces
+// and punctuation in determining if the string is a palindrome.
+// --- Examples:
+//   palindrome("abba") === true
+//   palindrome("abcdefg") === false
+
+function palindrome(str) {
+  let result = "";
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    result += str[i];
+  }
+  return str === result;
+}
