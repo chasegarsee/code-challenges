4cfb4f54d188e4287f310ddc34233afe3ccedb5a
chasegarsee <chasegarsee@gmail.com>
2019-09-22 16:25:18 -0500

    Flip And Invert Image

---
+++ LeetCode/FlipAndInvertAnImage.js
@@ -0,0 +1,5 @@
+var flipAndInvertImage = function(A) {
+  return A.map(image => image.reverse().map(b => (b === 0 ? 1 : 0)));
+};
+
+console.log(flipAndInvertImage([[1, 1, 0], [1, 0, 1], [0, 0, 0]]));
