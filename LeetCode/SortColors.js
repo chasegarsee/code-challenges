f318106612f7575391a7f2ef6540948db16ca5ff
chasegarsee <chasegarsee@gmail.com>
2019-09-23 14:22:55 -0500

    bug fix

--- LeetCode/SortColors.js
+++ LeetCode/SortColors.js
@@ -2,12 +2,13 @@ let sortColors = function(nums) {
   zero = [];
   one = [];
   two = [];
+  sorted = [];
   nums.map(color => {
     if (color === 0) zero.push(color);
     if (color === 1) one.push(color);
     if (color === 2) two.push(color);
   });
-  return nums.concat(zero, one, two);
+  return sorted.concat(zero, one, two);
 };
 
 console.log(sortColors([2, 0, 2, 1, 1, 0]));
