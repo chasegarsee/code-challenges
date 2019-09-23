1c550b6ecf89365816c438ad67b4921e69361d9e
chasegarsee <chasegarsee@gmail.com>
2019-09-23 14:29:09 -0500

    Sorted Colors in Place

--- LeetCode/SortColors.js
+++ LeetCode/SortColors.js
@@ -12,3 +12,23 @@ let sortColors = function(nums) {
 };
 
 console.log(sortColors([2, 0, 2, 1, 1, 0]));
+
+function sortColorsInPlace(nums) {
+  let sorted = false;
+  while (!sorted) {
+    sorted = true;
+    for (let i = 0; i < nums.length; i++) {
+      if (nums[i - 1] > nums[i]) {
+        sorted = false;
+        let temp = nums[i - 1];
+        nums[i - 1] = nums[i];
+        nums[i] = temp;
+      }
+    }
+  }
+  return nums;
+}
+
+let numbers = [2, 0, 2, 1, 1, 0];
+sortColorsInPlace(numbers);
+console.log(numbers);
