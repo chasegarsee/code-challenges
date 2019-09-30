e0c36fce4fe1408506732fc374f6d43753e07e51
chasegarsee <chasegarsee@gmail.com>
2019-09-29 21:22:01 -0500

    compare triplets

---
+++ HackerRank/compareTriplets.js
@@ -0,0 +1,15 @@
+
+
+function compareTriplets(a, b) {
+    let aPoints = 0
+    let bPoints = 0
+    for (let i = 0; i < a.length; i++) {
+        if (a[i] > b[i]) aPoints++
+        if (a[i] < b[i]) bPoints++
+    }
+    return [aPoints, bPoints]
+}
+
+
+
+console.log(compareTriplets([17, 28, 30], [99, 16, 8]))
