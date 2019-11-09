1f377973e79d4b761258f1b17bdc7b086c194d9c
chasegarsee <chasegarsee@gmail.com>
2019-11-08 21:08:03 -0600

    refactor

--- HackerRank/ClassesVsInstance.js
+++ HackerRank/ClassesVsInstance.js
@@ -1,16 +1,11 @@
 function Person(initialAge) {
-    // Add some more code to run some checks on initialAge
     if (initialAge < 0) {
         age = 0
         console.log("Age is not valid, setting age to 0.")
     } else {
         age = initialAge
     }
-
-
     this.amIOld = function () {
-        // Do some computations in here and print out the correct statement to the console
-
         if (age < 13) {
             console.log("You are young.")
         } else if (age >= 13 && age < 18) {
@@ -18,10 +13,8 @@ function Person(initialAge) {
         } else {
             console.log("You are old.")
         }
-
     };
     this.yearPasses = function () {
-        // Increment the age of the person in here
         age = age + 1
     };
 }
