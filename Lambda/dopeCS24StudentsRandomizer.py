6f28cd01d7ab9e74e1f598765d1cd3e15af72541
chasegarsee <chasegarsee@gmail.com>
2019-12-04 12:00:00 -0600

    Committed on

---
+++ Lambda/dopeCS24StudentsRandomizer.py
@@ -0,0 +1,18 @@
+from random import shuffle
+dopeCS24Students = [
+    "Aleksandra Foksman",
+    "Blaine Blonquist",
+    "Connor Claxton",
+    "Eudys Peralta Munoz",
+    "Jonathan Scott",
+    "Laura Don Diego",
+    "Michael Hart",
+    "Penny Tague",
+    "Sean Doyle",
+    "Javontay McElroy"
+]
+shuffle(dopeCS24Students)
+print(" !!YOINK!! ")
+for i in range(len(dopeCS24Students)):
+    print(i, dopeCS24Students[i])
+print("YEET    LETS GET IT!!")
