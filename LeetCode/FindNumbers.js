4fa01c13bb55d0ee0ecc0bc252ff61826aa119a4
chasegarsee <chasegarsee@gmail.com>
2020-01-15 12:00:00 -0600

    code challenges

--- LeetCode/FindNumbers.js
+++ LeetCode/FindNumbers.js
@@ -1,12 +1,12 @@
 function findNumbers(nums) {
   let even = 0;
 
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i].toString().length % 2 == 0) {
-      even.push(nums[i]);
+  nums.forEach(a => {
+    if (a.toString().length % 2 == 0) {
+      even++;
     }
-  }
-  return even.length;
+  });
+  return even;
 }
 numbers = [1, 22, 30, 1000];
 
