50d0f91b658dae9bf2d188c9932bf1834f1a095b
chasegarsee <chasegarsee@gmail.com>
2020-03-06 10:27:22 -0600

    pure recursion

--- DataStructures_Algorithms/07 Recursion/collectAllOdds.js
+++ DataStructures_Algorithms/07 Recursion/collectAllOdds.js
@@ -1,12 +1,27 @@
+/* HELPER METHOD RECURSION */
+
 function collectOddValues(arr) {
   let thatsOdd = [];
 
   function recursionHelper(helperInput) {
     if (helperInput.length === 0) return;
     if (helperInput[0] % 2 !== 0) thatsOdd.push(helperInput[0]);
-    recurssionHelper(helperInput.slice(1));
+    recursionHelper(helperInput.slice(1));
   }
   recursionHelper(arr);
   return thatsOdd;
 }
 console.log(collectOddValues([1, 2, 3, 4, 5, 6, 7, 8, 9]));
+
+/* PURE RECURSION */
+
+function collectOddValuesPure(arr) {
+  let veryOdd = [];
+  if (arr.length === 0) return veryOdd;
+  if (arr[0] % 2 !== 0) veryOdd.push(arr[0]);
+
+  veryOdd = veryOdd.concat(collectOddValuesPure(arr.slice(1)));
+  return veryOdd;
+}
+
+console.log(collectOddValuesPure([11, 12, 13, 14, 15, 16, 17, 18]));
