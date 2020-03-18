24c123b4e9af116bf3ae6cb728a396995e769d43
chasegarsee <chasegarsee@gmail.com>
2020-03-18 12:00:00 -0500

    Challenges

--- DataStructures_Algorithms/sorting/bubbleSort.js
+++ DataStructures_Algorithms/sorting/bubbleSort.js
@@ -11,4 +11,23 @@ function bubbleSortNaive(arr) {
   return arr;
 }
 
-console.log(bubbleSort([99, 34, 21, 53, 56, 67, 12, 76, 266, 54, 23, 76, 29]));
+console.log(
+  bubbleSortNaive([99, 34, 21, 53, 56, 67, 12, 76, 266, 54, 23, 76, 29])
+);
+
+function bubbleSortOptimized(arr) {
+  for (let i = arr.length; i > 0; i--) {
+    for (let j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+console.log(
+  bubbleSortOptimized([99, 34, 21, 53, 56, 67, 12, 76, 266, 54, 23, 76, 29])
+);
